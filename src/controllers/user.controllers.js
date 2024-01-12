14d27d38729b6ff46fc01aeb42f5177de22af0e2
Andres Mendoza <andres.david.mm@hotmail.com>
2024-01-11 22:51:18 -0500

    first commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,91 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const sendEmail = require('../utils/sendEmail');
+const EmailCode = require('../models/EmailCode');
+
+const getAll = catchError(async(req, res) => {
+    const results = await User.findAll();
+    return res.json(results);
+});
+
+const create = catchError(async(req, res) => {
+    const { email, password, firstName, lastName, country, image, frontBaseUrl } = req.body;
+    const encriptedPassword = await bcrypt.hash(password, 10);
+    const result = await User.create({
+        email,
+        password: encriptedPassword,
+        firstName,
+        lastName,
+        country,
+        image,
+    });
+
+    const code = require('crypto').randomBytes(32).toString("hex");
+    const link = `${frontBaseUrl}/auth/verify_email/${code}`;
+    await EmailCode.create({
+        code,
+        userId: result.id,
+    });
+
+    await sendEmail({
+        to: email,
+        subject: 'Email de verification',
+        html: `
+            <h1>Hola ${firstName} ${lastName}</h1>
+            <p>Gracias por iniciar sesión en user app</p>
+            <b>Usa este link para verificar tu email </b>
+            <hr>
+            ${link}
+        `,
+    });
+
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await User.findByPk(id);
+    if(!result) return res.sendStatus(404);
+    return res.json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: {id} });
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await User.update(
+        req.body,
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
+
+const verifyCode = catchError(async(req, res) => {
+    const { code } = req.params;
+    const emailCode = await EmailCode.findOne({ where: { code: code }});
+    if (!emailCode) return res.status(401).json({ message: "Codigo invalido" });
+    // const user = await User.findByPk(emailCode.userId);
+    // user.isVerified = true;
+    // await user.save();
+    const user = await User.update(
+        { isVerified: true }, 
+        { where: {id: emailCode.userId}, returning: true 
+    });
+    await emailCode.destroy();
+    return res.json(user[1][0]);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update,
+    verifyCode,
+}
